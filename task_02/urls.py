2865801cbf890c46fc91d61d60a4c06cce845060
Ali Ahmad <turgut@Alis-MacBook-Pro.local>
2022-04-08 23:04:26 +0300

    complete url to render template html file

--- task_02/urls.py
+++ task_02/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from restaurants import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',,name='hello-world'),
+    path('',views.render_a_template,name='hello-world'),
+    # path('render/', include('restaurants.urls')),
+
 ]
